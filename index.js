19ea85a8f283e6fae04d8fc730d68177eb46ef58
Littletsu <38116582+littletsu@users.noreply.github.com>
2022-09-06 16:03:46 -0500

    Path to URL Path & general improvements

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-try{const app = require('express')();
-const fs = require('fs');
-const os = require('os');
-const root = "/data/data/com.termux/files/home";
-const aliases = fs.readFileSync('alias', 'UTF8').split('\n').map(a => a.split(' '));
-console.log("Loaded " + aliases.length + " aliases")
-	
-const readDir = (path) => {
-	const readdir = fs.readdirSync(path);	
-	return readdir.map(f => {
-		const type = fs.lstatSync(`${path}/${f}`).isFile();
-		// 1/true = file, 0/false = dir
-		return {path, type, name: f}
-	});
-}
+try {
+	const app = require('express')();
+	const fs = require('fs');
+	const os = require('os');
+	const { join, basename } = require('path');
+	const mime = require('mime');
 
-app.set('view engine', 'ejs');
+	const root = "/";
+	const port = 3000;
 
-app.use((req, res, next) => {
-	
-	let found = false
- aliases.forEach(alias => {
-	if(alias.length !== 2) return
- 	if((req.url) === '/'+alias[0]) {
-		res.redirect('/?path='+alias[1])
-		found = true
-	}
- })
-	if(!found) return next()
-	
-})
+	const aliases = fs.readFileSync('alias', 'UTF8').split('\n').filter(line => line.trim() !== "").map(a => a.split(' '));
+	console.log("Loaded " + aliases.length + " aliases")
 
-app.get('/', (req, res) => {
-	let { path } = req.query;
-	if(!path) {
-		res.render('dir.ejs', {
-			files: readDir(root), 
-			path: root
+	const readDir = (path) => {
+		const readdir = fs.readdirSync(path);
+		return readdir.map(f => {
+			const type = fs.lstatSync(`${path}/${f}`).isFile();
+			// 1/true = file, 0/false = dir
+			return { path, type, name: f }
 		});
-	} else {
-		if(!path.startsWith(root)) path = root + path
-		if(fs.lstatSync(path).isDirectory()) {
-			
+	}
+
+	app.set('view engine', 'ejs');
+
+	app.use((req, res) => {
+		let found = false
+		aliases.forEach(alias => {
+			if (alias.length !== 2) return
+			if ((req.url) === '/' + alias[0]) {
+				res.redirect(alias[1])
+				found = true
+			}
+		})
+		if (found) return;
+		let path = decodeURI(req.url);
+		if ((path === "/") && (path !== root)) return res.redirect(root);
+
+		if (fs.lstatSync(path).isDirectory()) {
 			res.render('dir.ejs', {
 				files: readDir(path),
-				path
+				path, join
 			});
 		} else {
-			res.sendFile(path);
+			let data = fs.readFileSync(path);
+			let type = mime.getType(path);
+			if(!type || type === "text/plain") type = "text/plain;charset=utf-8";
+			res.writeHead(200, {
+				'Content-Type': type,
+				'Content-disposition': 'filename=' + basename(path),
+				'Content-Length': data.length
+			});
+			res.end(data, 'binary');
 		}
-	}
-});
+	})
 
+	app.listen(port, () => {
+		let ip = os.networkInterfaces().wlan0[0];
+		if(!ip) ip = "localhost";
+		else ip = ip.address;
+		console.log(`Server started at ${ip}:${port}`);
+	});
 
-app.listen(3000, () => {
-	const ip = os.networkInterfaces().wlan0[0].address;
-	console.log(`Server started at ${ip}:3000`);
-});
-
-}catch(err){console.log(err)} // for some reason termux wont let me see errors from node so i have to use this
+} catch (err) { console.log(err) } // for some reason termux wont let me see errors from node so i have to use this
